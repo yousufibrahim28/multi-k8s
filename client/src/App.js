6406af15d07430d3d7fd7bb3e50e01d50539c665
yousufibrahim28 <yousufibrahim28@gmail.com>
2023-06-24 22:39:32 +0530

    Made some changes

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Yousuf Ibrahim Just learned Kubernetes</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
